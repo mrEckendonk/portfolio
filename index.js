b617f4ad726c8c0575f5228c736dff487ebb8066
Abuya Obunga <vabuyia@gmail.com>
2021-11-16 00:31:33 +0300

    Add changes to the html and javascript file

---
+++ index.js
@@ -0,0 +1,15 @@
+const mainMenu = document.querySelector('.desktop-nav')
+const closeMenu = document.querySelector('.closeMenu')
+const openMenu = document.querySelector('.openMenu')
+
+openMenu.addEventListener('click', show)
+closeMenu.addEventListener('click', close)
+
+function show() {
+  mainMenu.style.display = 'flex'
+  mainMenu.style.top = '0'
+}
+
+function close() {
+  mainMenu.style.top = '-100%'
+}
