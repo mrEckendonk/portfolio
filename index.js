06d8d59afdb2eab6ac54901d37697dae8404e088
Eckybrazzz <mike.e@vecks.com.br>
2021-11-17 16:49:29 -0300

    add js to card

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const menuHamburgerBtn = document.querySelector('#hamburger');
+const popupMenu = document.querySelector('.popup);
 const closeBtn = document.querySelector('.close_btn');
 const popupCloseBtn = document.querySelector('.popup-close_btn');
 const mobileMenu = document.getElementById('mobileMenu');
@@ -7,10 +8,14 @@ const navLinks = document.querySelectorAll('.nav_link');
 function displayMenu() {
   mobileMenu.classList.toggle('hidden');
 }
+function displayPopup() {
+  popupMenu.classList.toggle('hidden');
+}
+
 
 menuHamburgerBtn.addEventListener('click', displayMenu);
 closeBtn.addEventListener('click', displayMenu);
-popupCloseBtn.addEventListener('click', displayMenu);
+popupCloseBtn.addEventListener('click', displayPopup);
 
 for (let i = 0; i < navLinks.length; i += 1) {
   navLinks[i].addEventListener('click', displayMenu);
